e28167a7518a69eb72a81348c9894c7a2c7ae224
Mat30-cmyk <betancurmateo116@gmail.com>
2026-04-21 13:55:34 -0500

    Segundo commit - estructura Sección 2 - Literales de Python

---
+++ seccion2/literales.py
@@ -0,0 +1,41 @@
+# Literales en Python
+
+# Cadenas vs números
+print("2")   # cadena
+print(2)     # entero
+
+# Enteros
+print(123)
+print(-100)
+print(11_111_111)
+
+# Octal y Hexadecimal
+print(0o123)   # octal → 83
+print(0x123)   # hexadecimal → 291
+
+# Flotantes
+print(2.5)
+print(-0.4)
+print(.4)
+print(4.)
+
+# Notación científica
+print(3E8)
+print(6.62607E-34)
+
+# Cadenas con comillas
+print("Me gusta \"Monty Python\"")
+print('Me gusta "Monty Python"')
+print('I\'m Monty Python.')
+
+# Booleanos
+print(True)
+print(False)
+print(True > False)
+print(True < False)
+
+# None
+print(None)
+
+#LAB  Literales de Python - Cadenas
+print("\"Estoy\"\"\"aprendiendo\"\"\"\"\"Python\"\"\"")
