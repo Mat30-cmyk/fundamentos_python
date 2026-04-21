aa3be27ba4b3e63fbaf8d54fa4e2ec49eb52aa86
Mat30-cmyk <betancurmateo116@gmail.com>
2026-04-21 13:46:23 -0500

    Primer commit - estructura proyecto Python

---
+++ seccion1/hola_mundo.py
@@ -0,0 +1,3 @@
+# Esta Linea imprime un saludo
+print("¡Hola, Mundo!")
+print("Tu nombre aquí")
---
+++ seccion1/print_argumentos.py
@@ -0,0 +1,4 @@
+print("Mi nombre es", "Teo")
+print("Mi nombre es", "Teo", end=" ")
+print("Python")
+print("Hola", "mundo", sep="-")
